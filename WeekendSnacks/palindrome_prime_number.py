55ec88a38d2f55a1838a0df77e603d334a60f113
Tosin <adeolamimilabake@gmail.com>
2026-05-09 18:21:14 +0100

    weekend snacks

--- WeekendSnacks/palindrome_prime_number.py
+++ WeekendSnacks/palindrome_prime_number.py
@@ -1,16 +1,17 @@
 def palindrome_number_check(number):
+    old_number = number
     new_number = 0
     while number > 0:
         digit = number % 10
         new_number = new_number * 10 + digit
         number = number // 10
-    if number == new_number:
+    if old_number == new_number:
         return True
     return False
    
 def prime_number_checker(number):
     counter = 0
-    for count in range(1, number):
+    for count in range(1, number+1):
         if number % count == 0:
             counter += 1
 
@@ -21,6 +22,6 @@ def prime_number_checker(number):
 def palindrome_prime_number_checker(number):
     check = palindrome_number_check(number)
     checker = prime_number_checker(number)
-    if check == checker:
+    if check == True and checker == True:
         return True
     return False
--- WeekendSnacks/palindrome_prime_number_test.py
+++ WeekendSnacks/palindrome_prime_number_test.py
@@ -5,7 +5,7 @@ class TestForCorrectDiscountPrice(unittest.TestCase):
 
     def test_that_number_is_both_palindrome_and_prime_number(self):
         
-        self.assertTrue(palindrome_prime_number.palindrome_prime_number_checker(131))
+        self.assertTrue(palindrome_prime_number.palindrome_prime_number_checker(373))
 
     def test_that_number_is_not_both_palindrome_and_prime_number(self):
         self.assertFalse(palindrome_prime_number.palindrome_prime_number_checker(121))
